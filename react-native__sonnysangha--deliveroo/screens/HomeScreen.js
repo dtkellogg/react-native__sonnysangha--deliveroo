99c41a9c4d58b996be6a427e22b55d0f18cbcbb0
Toshi <dtkellogg10@gmail.com>
2023-01-30 14:37:35 -0400

    Header - built

--- react-native__sonnysangha--deliveroo/screens/HomeScreen.js
+++ react-native__sonnysangha--deliveroo/screens/HomeScreen.js
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon
+} from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  })
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru'
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCB8" />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCB8" />
+        </View>
+      </Text>
+    </SafeAreaView>
   )
 }
 
