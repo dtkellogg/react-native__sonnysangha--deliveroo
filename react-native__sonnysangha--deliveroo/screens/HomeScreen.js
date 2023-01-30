621219af2bbce957445dc807f7c6c0b8c7dad882
Toshi <dtkellogg10@gmail.com>
2023-01-30 16:01:55 -0400

    Featured rows - component built out

--- react-native__sonnysangha--deliveroo/screens/HomeScreen.js
+++ react-native__sonnysangha--deliveroo/screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
   AdjustmentsVerticalIcon
 } from 'react-native-heroicons/outline'
 import Categories from '../components/Categories'
+import FeaturedRow from '../components/FeaturedRow'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
@@ -57,10 +58,25 @@ const HomeScreen = () => {
             paddingBottom: 100
           }}
         >
-          {/* Categories */}
-          <Categories />
+        {/* Categories */}
+        <Categories />
 
-          {/* Featured Rows */}
+        {/* Featured Rows */}
+        <FeaturedRow
+          id="1"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+        <FeaturedRow
+          id="2"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+        <FeaturedRow
+          id="3"
+          title="Offers near you!"
+          description="Why not support your local restaurant tonight!"
+        />
 
         </ScrollView>
     </SafeAreaView>
